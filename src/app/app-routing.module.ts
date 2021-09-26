e5576b7bc3a07d566c63d2f025723e8e1be37cdb
Arthur F <arthur.cdfreitas@gmail.com>
2021-09-25 21:52:49 -0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
+import { TreeComponent } from './components/tree/tree.component';
+import { TableComponent } from './components/table/table.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddressFormComponent } from './components/address-form/address-form.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DragDropComponent } from './components/drag-drop/drag-drop.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'address-form',
+    component: AddressFormComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'drag-drop',
+    component: DragDropComponent,
+  },
+  {
+    path: 'navigation',
+    component: NavigationComponent,
+  },
+  {
+    path: 'table',
+    component: TableComponent,
+  },
+  {
+    path: 'tree',
+    component: TreeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,68 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { LayoutModule } from '@angular/cdk/layout';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTreeModule } from '@angular/material/tree';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddressFormComponent } from './components/address-form/address-form.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DragDropComponent } from './components/drag-drop/drag-drop.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { TableComponent } from './components/table/table.component';
+import { TreeComponent } from './components/tree/tree.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressFormComponent,
+    NavigationComponent,
+    TableComponent,
+    DashboardComponent,
+    TreeComponent,
+    DragDropComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatTreeModule,
+    DragDropModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
